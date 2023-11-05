ae09fe116e88345001f22468f89fadd5ad403b0e
Murray <gary.murray1@volkswagen-groupservices.com>
2023-11-05 18:16:52 +0100

    JPA find by id

--- server/tech-blog-app-test/src/test/groovy/com/gmurray/tech/blog/post/infrastructure/out/persistence/jpa/PostJpaStorageAdapterIT.groovy
+++ server/tech-blog-app-test/src/test/groovy/com/gmurray/tech/blog/post/infrastructure/out/persistence/jpa/PostJpaStorageAdapterIT.groovy
@@ -2,10 +2,12 @@ package com.gmurray.tech.blog.post.infrastructure.out.persistence.jpa
 
 import com.gmurray.tech.blog.fixtures.persistence.BlogPostTestData
 import com.gmurray.tech.blog.fixtures.persistence.BlogPostTestDataCreator
+import com.gmurray.tech.blog.fixtures.persistence.TestPostCategory
 import com.gmurray.tech.blog.fixtures.slices.JpaDbTest
 import com.gmurray.tech.blog.post.application.exception.PostAuthorDoesNotExistException
 import com.gmurray.tech.blog.post.application.port.in.CreateBlogPostUseCase
 import com.gmurray.tech.blog.post.domain.Categories
+import com.gmurray.tech.blog.post.domain.PostId
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
 import spock.lang.Specification
@@ -36,7 +38,7 @@ class PostJpaStorageAdapterIT extends Specification {
         def command = new CreateBlogPostUseCase.NewBlogPostCommand(authorId,title,description,tags,categories)
 
         when:
-        def result = postJpaStorageAdapter.create(command)
+        postJpaStorageAdapter.create(command)
 
         then:
         thrown(PostAuthorDoesNotExistException)
@@ -51,13 +53,33 @@ class PostJpaStorageAdapterIT extends Specification {
         def description = "post description"
         def tags = ["Tag","Tag2"].toSet()
         def categories = [Categories.ENTERTAINMENT].toSet()
-        def command = new CreateBlogPostUseCase.NewBlogPostCommand(authorId,title,description,tags,categories)
+        def command = new CreateBlogPostUseCase.NewBlogPostCommand(authorId, title, description, tags, categories)
 
         when:
         def result = postJpaStorageAdapter.create(command)
 
         then:
         result > 0
+
+    }
+
+    def "getBlogPostById should return post details for valid unique identifier "() {
+        given:
+        def testPost = createPost()
+        def postId = new PostId(testPost.blogPost.id)
+
+        when:
+        def result = postJpaStorageAdapter.getBlogPostById(postId)
+
+        then:
+        result != null
+        result.title.value == testPost.blogPost.title
+        result.author.value() == testPost.blogAuthor.id
+        result.description.value == testPost.blogPost.description
+        result.categories.size() == testPost.blogPost.categories.size()
+        result.categories.forEach { pc ->
+            assert testPost.blogPost.categories.find { it.name() == pc.value }
+        }
 
     }
 
@@ -72,4 +94,18 @@ class PostJpaStorageAdapterIT extends Specification {
         return testData.blogAuthor.id
     }
 
+    def createPost(String title = "post title",
+                   String description = "post description",
+                   Set<TestPostCategory> categories = [TestPostCategory.SPORT, TestPostCategory.ENTERTAINMENT].toSet()
+
+    ) {
+        def testData = new BlogPostTestData()
+        testData.blogPost.title = title
+        testData.blogPost.description = description
+        testData.blogPost.categories = categories
+
+        blogPostTestDataCreator.createBlogPostWith(testData)
+
+        return testData
+    }
 }
